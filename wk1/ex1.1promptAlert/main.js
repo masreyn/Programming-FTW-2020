95a4bce9152af60629651c99898f9bb9703b732d
masreyn <49424309+masreyn@users.noreply.github.com>
2020-01-23 22:17:58 -0600

    Week 1 Exercises
    Week 1 Exercises

---
+++ wk1/ex1.1promptAlert/main.js
@@ -0,0 +1,13 @@
+//console.log('Hello World');
+//alert('Hello World');
+//Use console.log to print "Hello, <name goes here>" to the screen.
+//console.log("Hello," + name);
+//alert("Hello, " + name + "!")
+
+//Part1
+console.log('Hello World');
+
+//Part2 
+let name = prompt("What is your name?");
+console.log(`Hello, ${name}!!!`);
+//alert(`Hello, ${name}!!!`);
