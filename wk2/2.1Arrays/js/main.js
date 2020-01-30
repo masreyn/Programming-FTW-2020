e20cce3a5a43ca02c5359c04b2ef3a5f08799ca2
marsreynolds <schne821@umn.edu>
2020-01-30 17:14:08 -0600

    2.1 Array changes
    2.1 Array changes

--- wk2/2.1Arrays/js/main.js
+++ wk2/2.1Arrays/js/main.js
@@ -5,21 +5,28 @@
         4. add the movie to the array using push or create a new array with concat
         5. display full list of movies with toString() method
         */
+       
 myMoviesArray = ['Big Lebowski', 'Harry Potter', 'The Seventh Seal', 'Urgh', 'Singing in the Rain'];
 console.log(myMoviesArray);
 
 //add user input w/ array manipuation.  "push" - add items to the end
 let inputMovie = prompt('What was the last movie you watched?');
+
 myMoviesArray = [' The Big Lebowski ', 'Harry Potter ', 'The Seventh Seal ', 'Urgh ', 'Singing in the Rain '];
 console.log(myMoviesArray);
+
 //add user input w/ array manipuation.  "push" - add items to the end
 let inputMovie = prompt(' What was the last movie you watched?');
+
 myMoviesArray.push(inputMovie);
 console.log(myMoviesArray);
+
 //toString() method
         /*console.log(kensCar.toString());
         const myCars = ['rav', 'pilot', 'juke'];    
         console.log(myCars.toString());
-        */ 
+        */
+       
+console.log(myMoviesArray.toString());
 const allMovies = myMoviesArray.toString();
 alert('Here is the list of recently watched movies:' + allMovies);
