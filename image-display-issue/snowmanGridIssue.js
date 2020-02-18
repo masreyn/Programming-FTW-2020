02168ba6ae762a477182e0ebb04912a349645a6e
marsreynolds <schne821@umn.edu>
2020-02-17 19:15:46 -0600

    Image-display-issue
    Image-display-issue

---
+++ image-display-issue/snowmanGridIssue.js
@@ -0,0 +1,25 @@
+let grid = undefined;
+
+
+function setup(){
+  createCanvas(1000, 800);
+  background("#ccc");
+  grid = loadImage("images/100px_grid.png");
+}
+
+function draw() {
+  background(grid);
+  //snowman legs
+  fill("#f1f1f1");
+  stroke(100, 250, 100);
+}
+
+
+
+> p5.js says: It looks like there was a problem loading your image. 
+Try checking if the file path [https://editor.p5js.org//images/100px_grid.png] is correct, 
+hosting the image online, or running a local server.
+[https://github.com/processing/p5.js/wiki/Local-server] 
+Event {isTrusted: true, constructor: Object}
+isTrusted: true
+<constructor>: "Event"
