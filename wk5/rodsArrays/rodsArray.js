50855cf5e6cc94a647634c7011206073718696ce
marsreynolds <schne821@umn.edu>
2020-02-20 19:27:35 -0600

    Gram+ positive rods
    Gram positive rods- sort of

---
+++ wk5/rodsArrays/rodsArray.js
@@ -0,0 +1,35 @@
+
+let cells = [];
+
+function setup () {
+  createCanvas(600, 400);
+  for (var i = 0; i < 20; i++) {
+    cells[i] = new Cell(300, 200);
+  }
+}
+
+function draw() {
+  background('#00304A');
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].move();
+    cells[i].show();
+  }
+}
+
+class Cell {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move () {
+    this.x = this.x + random(-4, 4);
+    this.y = this.y + random(-4, 4);
+  }
+
+  show () {
+    noStroke();
+    fill('#ff45f3');
+    ellipse(this.x, this.y, 37, 18);
+  }
+}
