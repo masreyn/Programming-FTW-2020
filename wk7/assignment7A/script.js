e1d5c21e15038990aae42962a03e74104c0be3de
marsreynolds <schne821@umn.edu>
2020-03-09 00:06:59 -0500

    1st Commit did not commit
    1st Commit did not commit

--- wk7/assignment7A/script.js
+++ wk7/assignment7A/script.js
@@ -1,15 +1,15 @@
-// This assignment, your collection object should have 3 to 5 properties. 
-// Create an array of exactly 5 of these objects & include this array as one of the properties in your Vue data object.  
+/* Assign 7A create collection object that has 3 to 5 properties.
+Create an array of exactly 5 of these objects and include this array as one of the properties in your Vue data object. */
 
-// Guitar 1 = Object 1  "Mustang",
+// Guitar 1 = Object 1  "Mustang"
 const guitarArry = [
   {
     name: 'Mustang',
     maker: 'Fender',
     shape: 'Cutaway, solid',
     period: '1964-82',
-    pickups: '2-Single-coil',
-    players: 'Kurt Cobain, David Byrne, Nick Cave, Frank Black- Pixies, Thurston Moore- Sonic Youth.'
+    pickups: '2 Single-coil',
+    players: 'Kurt Cobain, David Byrne, Nick Cave, Frank Black - Pixies, Thurston Moore - Sonic Youth.'
   },
 
   // Guitar 2 = Object 2 "Flying V"
@@ -19,7 +19,7 @@ const guitarArry = [
     shape: 'V, solid',
     period: '1958-present',
     pickups: '2 Double-coil (humbucking)',
-    players: 'Albert King, Chuck Berry, Mike McCready- Pearl Jam.'
+    players: 'Albert King, Chuck Berry, Mike McCready - Pearl Jam.'
   },
 
   // Guitar 3 = Object 3  Bo Diddley ‘Big B’
@@ -29,9 +29,8 @@ const guitarArry = [
     shape: 'Rectangle, hollow',
     period: '1959-present',
     pickups: '2 Double-coil (humbucking)',
-    players: 'Bo Diddley, Jack Black- White Stripes'
+    players: 'Bo Diddley, Jack Black - White Stripes'
   },
-
   // Guitar 4 = Object 4 Teardrop Vox Mark III
   {
     name: 'Teardrop  Mark III',
@@ -39,7 +38,7 @@ const guitarArry = [
     shape: 'Teardrop, solid',
     period: '1963-present',
     pickups: '2-3 Single-coil',
-    players: 'The Mark VI was made for Brian Jones of The Rolling Stones.'
+    players: 'The Mark III was made for Brian Jones of The Rolling Stones.'
   },
 
   // Guitar 5 = Object 5. Mosrite Mark V Mosrite Mark V Ventures
@@ -48,8 +47,8 @@ const guitarArry = [
     maker: 'Mosrite',
     shape: 'Cutaway, solid',
     period: '1956-94',
-    pickups: '2-Single-coil',
-    players: 'Johnny Ramone, Ricky Wilson- The B-52s, Dave Alexander-The Stooges, Robert Smith-The Cure.'
+    pickups: '2 Single-coil',
+    players: 'Johnny Ramone, Ricky Wilson - The B-52s, Dave Alexander-The Stooges, Robert Smith -The Cure.'
   }
 ];
 
@@ -59,8 +58,8 @@ var myVue = new Vue({
     guitarsAr: guitarArry,
     guitarHeadlineAlign: 'center',
     guitarHeadline: {
-      color:'#3592a7,'
+      color: '#369ba3',
       fontSize: '3rem'
-    },
+    }
   }
 });
