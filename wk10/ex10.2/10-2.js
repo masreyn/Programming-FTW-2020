422619d7ec53068091b204d306c2860f40fd7cb8
marsreynolds <schne821@umn.edu>
2020-04-10 21:00:24 -0500

    10.2 | Exercise: Displaying external JSON data in Vue
    10.2 | Exercise: Displaying external JSON data in Vue

---
+++ wk10/ex10.2/10-2.js
@@ -0,0 +1,39 @@
+// Exercise 10.2 - Displaying external JSON data in Vue
+// INSTRUCTIONS:  Using the provided JSON file you will be creating a collection of music tracks.
+// You will use Axios to fetch the JSON file and then use Vue to display it.
+
+Vue.component('streaming-track', {
+  template: `<div v-bind:class="['track', trending]">
+        <h3>{{track.title}}</h3>
+        <div><img v-bind:src="track.cover" alt=""></div>
+        <small>{{track.artist}}</small>
+  </div>`,
+  props: ['track'],
+  computed: {
+    trending: function () {
+      const delta = this.track.rank - this.track.position.positionLastWeek;
+      if (delta > 0) {
+        return 'up';
+      } else if (delta < 0) {
+        return 'down';
+      } else {
+        return 'no-change'
+      }
+    }
+  }
+});
+const vm = new Vue({
+  el: '#musicApp',
+  data: {
+    tracks: []
+  },
+  mounted () {
+    axios
+      .get('./data/music-list.json')
+      .then(response => {
+        console.log('response', response);
+        vm.tracks = response.data;
+        console.log(vm.tracks);
+      });
+  }
+});
